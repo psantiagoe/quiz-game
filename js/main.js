23838c16a89c6248fc74177cb9820a721dc827c8
santiago.escobar <santiago.escobar1995@gmail.com>
2022-02-18 23:17:35 -0300

    Code cleaning

--- js/main.js
+++ js/main.js
@@ -288,14 +288,7 @@ let decodeEntities = (function () {
 })();
 
 function addPlayer(player) {
-	db.collection("players")
-		.add({
-			player,
-		})
-		.then((docRef) => {
-			console.log("Document written with ID: ", docRef.id);
-		})
-		.catch((error) => {
-			console.error("Error adding document: ", error);
-		});
+	db.collection("players").add({
+		player,
+	});
 }
